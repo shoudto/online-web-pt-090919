99c3a57b96ffb414e0ad545226ce91f2ca62f843
allisonkadel <akadel01@gmail.com>
2019-10-02 14:39:25 -0400

    Add comments to OO-Ruby-Intro BankAccount class

--- OO-Ruby-Intro/bank_account.rb
+++ OO-Ruby-Intro/bank_account.rb
@@ -1,30 +1,40 @@
+require 'pry' #=> requiring the pry gem will allow us to
+                # call binding.pry inside of our code
 
-require 'pry'
 class BankAccount
 
-    ACCOUNTS = []
-    # @@accounts = []
+    # We can use a constant or a class variable to keep
+    # track of all of the BankAccount instances in the 
+    # BankAccount class
 
-    # attr_accessor :balance
-    # attr_reader :balance
-    # attr_writer :balance
+    ACCOUNTS = [] #=> constant
+    # @@accounts = [] #=> class variable
+    
 
-    # id, balance
-    def initialize(id, balance)
+    # attr_accessor :balance #=> creates an attribute reader and writer for balance
+    # attr_reader :balance #=> creates an attribute reader for balance
+    # attr_writer :balance #=> creates an attribute writer for balance
+
+    # attributes: id, balance
+    def initialize(id, balance) #=> class constructor
         # binding.pry
+        # use pry to look at the value of self inside the initialize method
         ACCOUNTS << self
+        # Here, the value of self is the current BankAccount instance that is being created
         @id = id
         @balance = balance
     end
 
     # attr_reader :balance
-    def balance
+    # will generate the following method
+    def balance #=> attribute reader
         @balance
     end
 
-    # attr_writer :balance
-    # def balance=(balance)
-    #     @balance = balance
-    # end
+    # attr_writer :balance 
+    # will generate the following method
+    def balance=(balance) #=> attribute writer
+        @balance = balance
+    end
 
 end
